94c791cc9c87a542082a18ba23df5c7c03881ffb
Tri Nanda <2001020024@student.umrah.ac.id>
2024-10-20 22:52:33 +0700

    dah lah cape

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -120,6 +120,7 @@ class TaskController extends Controller
 
     // Mengupdate status task berdasarkan karyawan (dengan task_assignments)
     // Masih ada yang kurang
+    // Masih ada yang kurang
     public function updateTaskStatus(Request $request, Task $task) 
     {
         // Validasi input
